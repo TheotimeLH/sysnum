77027c42fcce9ca20276ae459b395989fc97ef60
Samuel Coulomb <84394386+Samsamitos@users.noreply.github.com>
2021-12-27 12:54:57 +0100

    Update alu.py

--- netlist_python/alu.py
+++ netlist_python/alu.py
@@ -1,18 +1,81 @@
 
 from lib_carotte import *
 
+# Logique
+    
+def n_Not(a):
+    s = ~a[0]
+    for i in range(1, 8):
+        s = s + ~a[i]
+    return s
+    
+def n_And(a, b):
+    s = a[0] & b[0]
+    for i in range(1, 8):
+        s = s + (a[i] & b[i])
+    return s
+   
+def n_Nand(a, b):
+    c = n_And(a, b)
+    return n_Not(c)
+    
+def n_Or(a, b):
+    s = a[0] | b[0]
+    for i in range(1, 8):
+        s = s + (a[i] | b[i])
+    return s
+    
+def n_Xor(a, b):
+    s = a[0] ^ b[0]
+    for i in range(1, 8):
+        s = s + (a[i] ^ b[i])
+    return s
+
+# Clock
+
+def nul(a):
+    b = Constant("1")
+    for i in range(8):
+        b = ~a[i] & b
+    return b
+
+def egal(a, b):
+    c = n_Xor(a, b)
+    return nul(c)
+
+def incr(a):
+    b = Constant("1")
+    s = a[0] ^ b
+    b = a[0] & b
+    for i in range(1, 8):
+        s = s + (a[i] ^ b)
+        b = a[i] & b
+    return s
+
+def incr_mod(a,b):
+    c = incr(a)
+    m = egal(b, c)
+    return Mux(m, c, 0)
+
+# Arithmétique
+
 def full_adder(a, b, c):
-    tmp = a ^ b
-    return (tmp ^ c, (tmp & c) | (a & b))
+    t = a ^ b
+    return (t ^ c, (t & c) | (a & b))
 
 def n_adder(a, b):
-    assert(a.bus_size == b.bus_size)
     c = Constant("0")
     (s, c) = full_adder(a[0], b[0], c)
-    for i in range(1, a.bus_size):
-        (s_i, c) = full_adder(a[i], b[i], c)
-        s = s + s_i
+    for i in range(1, 8):
+        (t, c) = full_adder(a[i], b[i], c)
+        s = s + t
     return (s, c)
-  
-def main(a,b,op):
-  
+
+def n_Neg(a):
+    b = n_Not(a)
+    return incr(b)
+
+# Main
+    
+def main(a, b, op):
+ 
