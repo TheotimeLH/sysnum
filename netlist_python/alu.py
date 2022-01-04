d040e91fdec94a7bb6ae4e522e4c5d8ca5bff7fb
Samuel Coulomb <84394386+Samsamitos@users.noreply.github.com>
2022-01-04 15:05:32 +0100

    main

--- netlist_python/alu.py
+++ netlist_python/alu.py
@@ -79,3 +79,26 @@ def n_Neg(a):
     
 def main(a, b, op):
 	
+	c0 = Constant("0"*16)
+	c1 = n_adder(a, b)
+	c2 = mult(a, b)
+	c3 = sub(a, b)
+	c4 = Neg16(a)
+	c5 = Not16(a)
+	c6 = And16(a, b)
+	c7 = Or16(a, b)
+	c8 = Xor16(a, b)
+	c9 = incr_mod(a, b)
+	
+	d0 = Mux(op[0], d1, d8)
+	d1 = Mux(op[1], d2, d3)
+	d2 = Mux(op[2], d4, d5)
+	d3 = Mux(op[2], d6, d7)
+	d4 = Mux(op[3], c0, c1)
+	d5 = Mux(op[3], c2, c3)
+	d6 = Mux(op[3], c4, c5)
+	d7 = Mux(op[3], c6, c7)
+	d8 = Mux(op[3], c8, c9)
+	d9 = Nul(d0)
+	
+	return d0, d9
