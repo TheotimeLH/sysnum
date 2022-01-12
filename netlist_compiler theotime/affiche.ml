ba8fc799c06d48eb8ca75f0dcd9ab3ab339b629c
samuel <samuel.coulomb@gmail.com>
2022-01-12 19:00:37 +0100

    base

--- netlist_compiler theotime/affiche.ml
+++ netlist_compiler theotime/affiche.ml
@@ -1,4 +1,23 @@
 
-let affiche_batons s (* secondes *) mi (* minutes *) h (* heures *) 
-  j (* jours *) mo (* mois *) a1 (* 2 bits poids forts année *) a2 (* 2 autres *) =
-  ()
+open Graphics
+
+let print16 x n = (* A COMPLETER *)
+
+let affiche_batons
+  s (* secondes *)
+  mi (* minutes *)
+  h (* heures *) 
+  j (* jours *)
+  mo (* mois *)
+  al (* millénaire + centenaire *) 
+  ar (* décennie + année *) =
+
+  open_graph " 2300x400" ;
+  print16 100 s ;
+  print16 400 mi ;
+  print16 500 h ;
+  print16 1000 j
+  print16 1300 mo ;
+  print16 1600 al ;
+  print16 1900 ar ;
+  close_graph ()
